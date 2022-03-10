5e53d9a7e5aab2bbf421080b7b8946cdf908c3e0
Rachamallu11 <91821123+Rachamallu11@users.noreply.github.com>
2022-03-10 22:19:29 +0530

    Add files via upload

---
+++ CustomerDao.java
@@ -0,0 +1,11 @@
+package day5assign2;
+
+import java.util.List;
+public interface CustomerDao { 
+		public Customer findCustomerById(int id);
+		public String save(Customer c);
+		public String delete(int customerId);
+		public List<Customer> listAll();
+		public String update(int id, Customer c);
+
+	}
